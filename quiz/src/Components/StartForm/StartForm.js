71a4dec447884a96793ce448ab4f3a9476302af5
Elvira <sabirova.elvira.gam@gmail.com>
2023-02-21 10:19:00 +0500

    init: create react-app structure

---
+++ quiz/src/Components/StartForm/StartForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './QuizForm.css'
+
+export default function QuizForm() {
+  return (
+    <form onSubmit={}>
+      <h2>
+        Train your skills
+        <select></select>
+        {/*  category, difficult, maskedInput count q, start button*/}
+        <button type='submit'></button>
+      </h2>
+    </form>
+  )
+}
