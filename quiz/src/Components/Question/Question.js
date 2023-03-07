05d5776704873694f51585e3e6b3f7379d0b1811
Elvira <sabirova.elvira.gam@gmail.com>
2023-03-07 09:35:23 +0500

    feat: add styles for answers

--- quiz/src/Components/Question/Question.js
+++ quiz/src/Components/Question/Question.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './Questioin.css'
+import './Questioin.css';
 
 export default function Question({ q }) {
   return (
     <><h3>{q.question}</h3>
-      <fieldset className="answers">
-        {Object.keys(q.answers).map(key => q.answers[key] !== null ? (<label>{q.answers[key]}
-          <input name={q.id} type="radio" value={key}></input></label>) : '')}
+      <fieldset>
+        <ul className="answers">
+          {Object.keys(q.answers).map(key => q.answers[key] !== null ? (
+              <li className="answer" key={`${q.id}${key}`}>
+                <label className="answer__label">
+                  <input className="answer__input" name={q.id} type="radio" value={key}></input>
+                  <span className="answer__img"></span>
+                  <span className="answer__text">{q.answers[key]}</span>
+                </label>
+              </li>)
+            : '')}
+        </ul>
       </fieldset>
     </>
   );
